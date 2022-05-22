345eed1d84999d78816bc96d3686aab687f25d63
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-05-22 15:27:54 -0500

    Creación del proyecto

---
+++ src/main/java/com/example/metroTickets/MetroTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.metroTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/metroTickets/MetroTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metroTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
